bde0261cb6fd1d543cc019f8f8185d321bcef5c7
hrid1 <hridoycse1@gmail.com>
2026-02-21 10:30:53 +0600

    Fix done

--- app/(main)/coa-doc/page.tsx
+++ app/(main)/coa-doc/page.tsx
@@ -1,5 +1,5 @@
 import CoaDocHero from '@/components/pages/CoaDoc/CoaDocHero'
-import HowItworks from '@/components/pages/VerifyReport/HowItworks'
+import SimpleCOA from '@/components/pages/CoaDoc/SimpleCOA'
 import ContactSection from '@/components/pages/Homepage/ContactSection'
 
 
@@ -8,7 +8,9 @@ export default function page() {
   return (
     <div className='mt-[80px] sm:mt-[96px] mb-[40px]'>
       <CoaDocHero />
-      <HowItworks />
+      <SimpleCOA />
+
+      this is simple coa doc page
       <ContactSection />
     </div>
   )
